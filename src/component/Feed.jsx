13fe9d319eafdce619751f0c70c979e11bf14b39
sneha2395 <agarwal.sneha.231995@gmail.com>
2023-08-24 20:09:27 +0530

    Sidebar done

--- src/component/Feed.jsx
+++ src/component/Feed.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import {useState, useEffect} from 'react';
+import { Stack, Box, Typography} from '@mui/material';
+import Sidebar from './Sidebar';
+
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+     <Stack sx={{flexDirection:{sx:"column", md: "row"}}}>
+      <Box sx ={{height:{sx:"auto", md: '92vh'}, borderRight:'1px solid #3d3d3d',px: {sx:0, md:2}}}>
+    
+      
+       <Sidebar/>
+      <Typography className='copyright' variant='copy2' sx={{mt:'1.5', color:'#fff'}}>
+        CopyRight 2022@
+      </Typography>
+      </Box>
+     </Stack>
   )
 }
 
