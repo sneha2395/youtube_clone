af8225e0f5a8af8a112ab4f049e9ff943edf65e7
sneha2395 <agarwal.sneha.231995@gmail.com>
2023-09-04 08:19:07 +0530

    Loader and  VideoDetail done

--- src/component/ChannelCard.jsx
+++ src/component/ChannelCard.jsx
@@ -31,7 +31,7 @@ const ChannelCard = ({ channelDetail, marginTop }) => (
         </Typography>
         {channelDetail?.statistics?.subscriberCount && (
           <Typography sx={{ fontSize: '15px', fontWeight: 500, color: 'gray' }}>
-            {parseInt(channelDetail?.statistics?.subscriberCount).toLocaleString()} Subscribers
+            {parseInt(channelDetail?.statistics?.subscriberCount).toLocaleString('en-US')} Subscribers
           </Typography>
         )}
       </CardContent>
