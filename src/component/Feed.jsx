b12e9505a8e154ac1e5343309a26211d709aea6b
sneha2395 <agarwal.sneha.231995@gmail.com>
2023-09-04 08:20:08 +0530

    Loader and VideoDetail done

--- src/component/Feed.jsx
+++ src/component/Feed.jsx
@@ -1,41 +1,41 @@
-import {useState, useEffect} from 'react';
-import { Stack, Box, Typography} from '@mui/material';
-import Sidebar from './Sidebar';
+import React, { useEffect, useState } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+
+import { fetchFromApi } from "../utils/fetchFromApi";
 import Videos from './Videos';
-import { fetchFromApi } from '../utils/fetchFromApi';
+import Sidebar from './Sidebar';
 
 
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState(null);
 
-  const [selectedCategory, setSelectedCategory] = useState('New');
-  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    setVideos(null);
 
-   useEffect(() =>{
     fetchFromApi(`search?part=snippet&q=${selectedCategory}`)
-    .then ((data) => setVideos(data.items))
-    
-   }, [selectedCategory]);
+      .then((data) => setVideos(data.items))
+    }, [selectedCategory]);
+
   return (
-     <Stack sx={{flexDirection:{sx:"column", md: "row"}}}>
-      <Box sx ={{height:{sx:"auto", md: '92vh'}, borderRight:'1px solid #3d3d3d',px: {sx:0, md:2}}}>
-    
-      
-       <Sidebar
-       selectedCategory={selectedCategory}
-       setSelectedCategory={setSelectedCategory}
-       />
-      <Typography className='copyright' variant='copy2' sx={{mt:'1.5', color:'#fff'}}>
-        CopyRight 2022@
-      </Typography>
-      </Box>
-      <Box p={2} sx={{overflow:'auto', height:'90vh', flex: '2 '}}>
-        <Typography variant='h4' fontWeight= "bold" mb={2} sx={{color:'white'}}> {selectedCategory}
-          <span style={{color:'#F31503'}}> videos</span>
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
+        <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
+        
+        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
+          Copyright © 2022 JSM Media
         </Typography>
-        <Videos videos={videos}/>
       </Box>
-     </Stack>
-  )
-}
+
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+          {selectedCategory} <span style={{ color: "#FC1503" }}>videos</span>
+        </Typography>
+
+        <Videos videos={videos} />
+      </Box>
+    </Stack>
+  );
+};
 
 export default Feed;
